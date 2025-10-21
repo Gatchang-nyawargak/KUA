93cbcb7f3d0041788820965a6e7840323b9a24fc
Gatchang-nyawargak <158443804+Gatchang-nyawargak@users.noreply.github.com>
2025-10-21 14:05:55 +0300

    made changes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import paymentRoutes from './routes/payment.js';
+import paymentRoutes from './routes/payment';
 
 dotenv.config();
 
